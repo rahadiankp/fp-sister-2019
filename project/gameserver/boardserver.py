d49917950bf0ce7ea4cc2650d95114e5902a68cc
rahadiankp <rahadian.k.putra@gmail.com>
2019-12-05 19:50:05 +0700

    wip server board 2

--- project/gameserver/boardserver.py
+++ project/gameserver/boardserver.py
@@ -7,7 +7,7 @@ class Board(object):
         self.game_started = False
         self.game_end_message = None  # "WIN 0" -> player 1 wins, "WIN 1" -> player 2 wins
         self.player_name_list = []  # 0 -> X, 1 -> O
-        self.is_player_1_turn = True
+        self.is_player_1_turn = 0
         self.board_data = [
             ["-", "-", "-"],
             ["-", "-", "-"],
@@ -16,6 +16,7 @@ class Board(object):
 
     def reset_board(self):
         self.game_started = False
+        self.game_end_message = None
         self.player_name_list = []
         self.is_player_1_turn = 0
         self.board_data = [
@@ -24,7 +25,7 @@ class Board(object):
             ["-", "-", "-"],
         ]
 
-    def check_player_status(self, player_name):
+    def check_status(self, player_name):
         player_index = -1
         try:
             player_index = self.player_name_list.index(player_name)
@@ -37,7 +38,9 @@ class Board(object):
 
         # check if victory has been achieved
         if not self.game_end_message:
-            return False, player_index, self.game_end_message
+            game_end_message = self.game_end_message
+            self.reset_board()
+            return False, player_index, game_end_message
 
         player_turn = not bool(player_index)
         if player_turn != self.is_player_1_turn:
@@ -58,18 +61,36 @@ class Board(object):
     def switch_turn(self):
         self.is_player_1_turn = not self.is_player_1_turn
 
+    def check_winning(self):
+        for ((x1, y1), (x2, y2), (x3, y3)) in self.winpos_list:
+            if self.board_data[y1][x1] == self.board_data[y2][x2] == self.board_data[y3][x3]:
+                piece_winning = self.board_data[y1][x1]
+                piece_index = self.pieces.index(piece_winning)
+                self.game_end_message = "WIN {0}".format(piece_index)
+                return True, self.game_end_message
+
+        return False
+
+    def check_player_status(self, player_name):
+        return self.check_status(player_name)
+
     def make_move(self, player_name, x, y) -> str:
-        status, player_index, msg = self.check_player_status(player_name)
+        status, player_index, msg = self.check_status(player_name)
         if not status:
             return msg
 
         # check if coord has piece already
         if self.board_data[y][x] != "-":
-            return False
+            return "BAD"
 
         piece = self.pieces[player_index]
         self.board_data[y][x] = piece
 
         self.switch_turn()
 
-        return True
+        # immediately send winning msg to winning player after the move
+        with self.check_winning() as (status, msg):
+            if status:
+                return msg
+
+        return "OK {0} {1},{2}".format(piece, x, y)
