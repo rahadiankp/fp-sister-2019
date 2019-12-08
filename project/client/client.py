20688506e165e6d0762e70d76512e7aaac6d73b9
rahadiankp <rahadian.k.putra@gmail.com>
2019-12-08 22:38:48 +0700

    init spectator

--- project/client/client.py
+++ project/client/client.py
@@ -12,21 +12,27 @@ class Client:
     board = None
     drawer = None
 
-    def __init__(self, username, proxy_uri):
+    def __init__(self, username, proxy_uri, spectator_mode=False):
         self.username = username
         try:
             self.proxy = Pyro4.Proxy(proxy_uri)
         except:
             print("Invalid URI")
             return
-        register_response: dict = self.proxy.push_command("START " + username)
-        if register_response['status'] != "OK":
-            raise Exception(register_response['message'])
-        self.board_id = register_response['board_id']
-        self.player_id = register_response['player_id']
+        if not spectator_mode:
+            register_response: dict = self.proxy.push_command("START " + username)
+            if register_response['status'] != "OK":
+                raise Exception(register_response['message'])
+            self.board_id = register_response['board_id']
+            self.player_id = register_response['player_id']
+        else:
+            self.board_id = "X"
+            self.player_id = "X"
+
         latest_board: dict = self.proxy.push_command("UPDATE")
         self.board = board.Board(latest_board['data'])
-        self.drawer = drawer.Drawer(self.board, self.username, str(self.board_id), self.player_id, self.proxy)
+        self.drawer = drawer.Drawer(self.board, self.username, str(self.board_id),
+                                    self.player_id, self.proxy, spectator_mode)
 
     def start(self):
         self.drawer.update_screen()
@@ -35,17 +41,20 @@ class Client:
 if __name__ == "__main__":
     USERNAME = ""
     PROXY_URI = ""
+    SPECTATOR = False
 
-    options, misc = getopt.getopt(sys.argv[1:], "u:h:",
-                                  ["username=", "host="])
+    options, misc = getopt.getopt(sys.argv[1:], "u:h:s",
+                                  ["username=", "host=", "spectator"])
     for opt, val in options:
         if opt in ["-u", "--username"]:
             USERNAME = val
         elif opt in ["-h", "--host"]:
             PROXY_URI = val
-    # client = Client(USERNAME, PROXY_URI)
+        elif opt in ["-s", "--spectator"]:
+            SPECTATOR = True
+    client = Client(USERNAME, PROXY_URI, SPECTATOR)
     # client = Client("receh", "PYRONAME:proxyserver-3@localhost:8888")
-    client = Client("alcredo", "PYRONAME:proxyserver-1@localhost:8888")
+    # client = Client("alcredo", "PYRONAME:proxyserver-1@localhost:8888")
     # client = Client("teje", "PYRONAME:proxyserver-2@localhost:8888")
     # client = Client("alfian", "PYRONAME:proxyserver-1@localhost:8888")
     client.start()
