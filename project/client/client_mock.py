e19d49106e8fdc6d889d892e6bb0331e53009619
rahadiankp <rahadian.k.putra@gmail.com>
2019-12-08 13:07:31 +0700

    remove annotation from client mock game server

--- project/client/client_mock.py
+++ project/client/client_mock.py
@@ -1,9 +1,9 @@
 import Pyro4
-from project.proxy.proxy_server_api import ProxyServerApi
+
 
 if __name__ == "__main__":
 
-    proxy: ProxyServerApi = Pyro4.Proxy('PYRONAME:proxyserver@localhost:8888')
+    proxy = Pyro4.Proxy('PYRONAME:proxyserver-1@localhost:8888')
     
     while True:
         command = input()
