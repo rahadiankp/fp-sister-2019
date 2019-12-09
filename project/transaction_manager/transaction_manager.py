848de44538177fc41262bf92048ff7db08cefd6c
rahadiankp <rahadian.k.putra@gmail.com>
2019-12-09 22:43:32 +0700

    support for tm failover and replication

--- project/transaction_manager/transaction_manager.py
+++ project/transaction_manager/transaction_manager.py
@@ -7,15 +7,22 @@ import transaction_manager_api
 
 class TransactionManager:
 
-    def __init__(self, name, host, port):
+    def __init__(self, name, host, port, bootstrap, proxy_uri_list):
         self.host = host
         self.port = port
         self.name = name
+        self.bootstrap = bootstrap
+        self.proxy_uri_list = proxy_uri_list
 
     def start(self):
         daemon = Pyro4.Daemon(host=self.host)
         ns = Pyro4.locateNS(self.host, self.port)
-        api = Pyro4.expose(transaction_manager_api.TransactionManagerApi)
+        # api = Pyro4.expose(transaction_manager_api.TransactionManagerApi)
+        api = transaction_manager_api.TransactionManagerApi(
+            "PYRONAME:" + self.name + "@" + self.host + ":" + str(self.port),
+            self.proxy_uri_list,
+            self.bootstrap
+        )
         uri_tm = daemon.register(api)
         ns.register("{}" . format(self.name), uri_tm)
         print(uri_tm)
@@ -26,9 +33,10 @@ if __name__ == '__main__':
     NAME = ""
     HOST = ""
     PORT = 0
+    BOOTSTRAP = False
 
-    options, misc = getopt.getopt(sys.argv[1:], "n:h:p:",
-                                  ["name=", "host=", "port="])
+    options, misc = getopt.getopt(sys.argv[1:], "n:h:p:b",
+                                  ["name=", "host=", "port=", "bootstrap"])
     for opt, val in options:
         if opt in ["-n", "--name"]:
             NAME = val
@@ -36,5 +44,12 @@ if __name__ == '__main__':
             HOST = val
         elif opt in ["-p", "--port"]:
             PORT = int(val)
-    TransactionManager(NAME, HOST, PORT).start()
+        elif opt in ["-b", "--bootstrap"]:
+            BOOTSTRAP = True
+    proxy_list = [
+        "PYRONAME:proxyserver-1@localhost:8888",
+        "PYRONAME:proxyserver-2@localhost:8888",
+        "PYRONAME:proxyserver-3@localhost:8888"
+    ]
+    TransactionManager(NAME, HOST, PORT, BOOTSTRAP, proxy_list).start()
 
--- project/transaction_manager/transaction_manager_api.py
+++ project/transaction_manager/transaction_manager_api.py
@@ -2,47 +2,98 @@ import Pyro4
 import hashlib
 
 
+@Pyro4.behavior(instance_mode="session")
 class TransactionManagerApi:
 
-    data = []
-    data_len = 0
+    def __init__(self, own_uri, proxy_uri_list, is_bootstrap):
+        self.own_uri = own_uri
+        self.proxy_uri_list = proxy_uri_list
+        self.data = []
+        self.data_len = 0
 
-    """
-    :type data: dict @see util.CommandResolver
-    """
-    @staticmethod
+        self.is_bootstrap = is_bootstrap
+        self.ready = False
+
+        self.register_to_proxy()
+
+    def register_to_proxy(self):
+        for uri in self.proxy_uri_list:
+            try:
+                proxy = Pyro4.Proxy(uri)
+                proxy.register_tm(self.own_uri)
+            except:
+                pass
+
+        if self.is_bootstrap:
+            self.ready = True
+            return
+
+        tm_uri_peer = None
+        for uri in self.proxy_uri_list:
+            try:
+                proxy = Pyro4.Proxy(uri)
+                tm_uri_peer = proxy.get_transaction_manager_uri()
+                break
+            except:
+                pass
+
+        up_to_date = False
+        from_index = 0
+        tm_peer_proxy = Pyro4.Proxy(tm_uri_peer)
+        commands = tm_peer_proxy.get_data_to_last_from(from_index)
+        latest_len = len(commands)
+
+        while not up_to_date:
+            for command in commands:
+                self.push_command(command)
+
+            new_latest_len = tm_peer_proxy.get_data_length()
+            up_to_date = new_latest_len == latest_len
+            from_index = latest_len - 1
+            latest_len = new_latest_len
+            if not up_to_date:
+                commands = tm_peer_proxy.get_data_to_last_from(from_index)
+
+        self.ready = True
+
+    @Pyro4.expose
+    def is_ready(self):
+        return self.ready
+
     @Pyro4.oneway
-    def push_command(command_data):
-        TransactionManagerApi.data.append(command_data)
-        TransactionManagerApi.data_len += 1
-        # print(TransactionManagerApi.data)
+    @Pyro4.expose
+    def push_command(self, command_data):
+        self.data.append(command_data)
+        self.data_len += 1
 
-    @staticmethod
-    def get_all_data():
-        return TransactionManagerApi.data
+    @Pyro4.expose
+    def get_all_data(self):
+        return self.data
 
-    @staticmethod
-    def get_data_length():
-        return TransactionManagerApi.data_len
+    @Pyro4.expose
+    def get_data_length(self):
+        return self.data_len
 
-    @staticmethod
-    def get_data_to_last_from(index):
-        return TransactionManagerApi.data[index:]
+    @Pyro4.expose
+    def get_data_to_last_from(self, index):
+        return self.data[index:]
 
-    @staticmethod
-    def ping():
+    @Pyro4.expose
+    def ping(self):
         return "PONG"
 
     """Debug methods
 
     """
 
-    @staticmethod
-    def verbose_server():
-        length = TransactionManagerApi.data_len
-        length_data = len(TransactionManagerApi.data)
+    @Pyro4.expose
+    def verbose_server(self):
+        length = self.data_len
+        length_data = len(self.data)
+        is_bootstrap = self.is_bootstrap
+        ready = self.ready
         hash_value = hashlib.md5("EMPTY TRANSACTION".encode())
-        for data in TransactionManagerApi.data:
+        for data in self.data:
             print(data)
             hash_value = hashlib.md5((hash_value.hexdigest() + str(data)).encode())
 
@@ -50,5 +101,7 @@ class TransactionManagerApi:
             'len': length,
             'len_data': length_data,
             'hash': hash_value.hexdigest(),
+            'ready': ready,
+            'is_bootstrap': is_bootstrap,
         }
 
